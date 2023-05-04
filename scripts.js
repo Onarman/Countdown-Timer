210c31623e1e8775947c4dfe65544bb893e2931b
onarman <onarmannn@gmail.com>
2023-05-04 11:40:56 +0300

    function done

---
+++ scripts.js
@@ -0,0 +1,24 @@
+let countdown;
+
+const timer = (seconds) => {
+	const now =  Date.now();
+	const then = now + seconds * 1000;
+	console.log({now,then});
+	displayTimeLeft(seconds)
+
+	countdown = setInterval(()=>{
+		const secondsLeft = Math.round((then - Date.now())/1000);
+		if(secondsLeft <= 0){
+			clearInterval(countdown);
+			return;
+		}
+
+		displayTimeLeft(secondsLeft)
+	},1000)
+}
+
+function displayTimeLeft(seconds) {
+	console.log(seconds);
+}
+
+timer(5);
